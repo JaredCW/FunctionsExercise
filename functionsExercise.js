3e76fd2bcfdc740130b2ddbc92809a3c5202f5ad
Jared Conner <jared.w.conner@gmail.com>
2022-10-31 09:40:10 -0500

    Finished exercise

---
+++ functionsExercise.js
@@ -0,0 +1,129 @@
+// 1
+
+function fozzieBear() {
+    console.log(`Wocka Wocka!`);
+}
+fozzieBear();
+
+// 2
+
+function beaker(speak) {
+    console.log(speak);
+    console.log(speak);
+    console.log(speak);
+    console.log(speak);
+}
+
+beaker();
+beaker(`Meep`);
+
+// 3
+
+function muppetShow(a, b) {
+    if (a === `Muppet` && b === `Show`){
+        console.log(`It's time to play the music. It's time to light the lights.`);
+    }
+}
+
+muppetShow(`Muppet`, `Show`);
+
+// 4
+
+function kermit() {
+    console.log(`Kermit the Frog`);
+}
+kermit();
+
+// 5
+function muppetShowSeasons(seasons) {
+    if (seasons === 5){
+        return true;
+    }
+    return false;
+}
+
+console.log(muppetShowSeasons(5));
+
+// 6
+const manOrMuppet = function() {
+    console.log(`Am I a man or am I a Muppet?`);
+}
+manOrMuppet();
+
+// 7
+rainbowConnection = () => `Someday we'll find it. The Rainbow Connection. The lovers, the dreamers, and me`;
+
+console.log(rainbowConnection());
+
+// 8
+// No
+
+// 9
+// Yes
+
+// 10a
+newMuppetMovies = [
+    `The Muppets`,
+    `Muppets Most Wanted`
+];
+
+// 10b
+const upperMovies = newMuppetMovies.map(movies => movies.toUpperCase());
+
+
+
+
+// BONUS
+// 11a
+ const oldMuppetMovies = [
+    `The Muppet Movie`, 
+    `The Muppets Take Manhattan`, 
+    `The Great Muppet Caper`, 
+    `The Muppet Christmas Carol`, 
+    `Muppet Treasure Island`, 
+    `Muppets From Space`
+];
+
+// 11b
+const twoMovies = oldMuppetMovies.filter(a => a.length === 22);
+console.log(twoMovies);
+
+// 12a
+const charactersOne = [`Statler`, `Waldorf`];
+
+// 12b
+const charactersTwo = [`The Swedish Chef`, `Animal`, `Rowlf`];
+
+// 12c
+randomMuppet = (characters) => {
+    console.log(`${characters[Math.floor(Math.random() * characters.length)]}`);
+}
+
+// 12d
+randomMuppet(charactersOne);
+randomMuppet(charactersTwo);
+
+
+// 13
+caps = (string) => {
+    let newString = ``;
+    for (i = 0; i < string.length; i++){
+        if (i % 2 === 0) {
+            newString += string[i].toUpperCase();
+        }
+    }
+    return newString
+}
+
+console.log(caps(`Hello World`));
+
+//EXTRA
+caps = (string) => {
+    let newString = ``;
+    for (i = 0; i < string.length; i++){
+        if (i % 2 === 0) {
+            newstring += string[i].toUpperCase();
+        }
+    }
+    return newStringl
+}
